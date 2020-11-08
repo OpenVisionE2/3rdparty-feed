bedd0baa04796bca9bddb6ce647400f27d47214a
persianpros <persianpros@gmail.com>
2020-11-09 00:13:17 +0330

    Faster fetch for TS like PLi

--- feed/get-ts.sh
+++ feed/get-ts.sh
@@ -2,10 +2,10 @@
 
 mkdir -p ts
 cd ts
-wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins/
-wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_IPTV/
-wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_Backup/
-wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_Multiboot/
+wget -c -nH -r -q -l0 -A ipk -np -nd --wait=1 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins/
+wget -c -nH -r -q -l0 -A ipk -np -nd --wait=1 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_IPTV/
+wget -c -nH -r -q -l0 -A ipk -np -nd --wait=1 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_Backup/
+wget -c -nH -r -q -l0 -A ipk -np -nd --wait=1 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_Multiboot/
 mv -f *.ipk ../
 cd ..
 rm -rf ts
