df22bf732e8ae0c9b5cc845b2ec5435c815ee82e
persianpros <persianpros@gmail.com>
2020-11-08 19:10:49 +0330

    Make sure we move the files as wget won't force

--- pli.sh
+++ pli.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p pli
+cd pli
 wget http://downloads.openpli.org/feeds/openpli-7-release/3rd-party/Packages.gz
 gzip -d Packages.gz
 find -type f -name "*Packages*" | xargs -L1 sed -i '/^Filename: /p'
@@ -7,3 +9,5 @@ sed -n '/^Filename: /p' Packages > Packages-files
 rm -f Packages
 find -type f -name "*Packages*" | xargs -L1 sed -i 's|Filename: |http://downloads.openpli.org/feeds/openpli-7-release/3rd-party/|g'
 wget --wait=1 -i Packages-files
+mv -f *.ipk ../
+cd ..
