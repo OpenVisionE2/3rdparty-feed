3bc442688d1412d0d7414f3e4b743f8a91840f6c
Persian Prince <persianpros@yahoo.com>
2020-11-09 02:32:37 +0330

    Lets have modules

---
+++ feed/get-ts.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir -p ts
+cd ts
+wget -c -nH -r -q -l0 -A ipk -np -nd --wait=1 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins/
+wget -c -nH -r -q -l0 -A ipk -np -nd --wait=1 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_IPTV/
+wget -c -nH -r -q -l0 -A ipk -np -nd --wait=1 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_Backup/
+wget -c -nH -r -q -l0 -A ipk -np -nd --wait=1 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_Multiboot/
+mv -f *.ipk ../
+cd ..
+rm -rf ts
