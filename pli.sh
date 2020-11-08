c1130a03d7f92eddd7479e4b53bfb9c390487f43
persianpros <persianpros@gmail.com>
2020-11-08 19:00:54 +0330

    Support PLi feed

---
+++ pli.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+wget http://downloads.openpli.org/feeds/openpli-7-release/3rd-party/Packages.gz
+gzip -d Packages.gz
+find -type f -name "*Packages*" | xargs -L1 sed -i '/^Filename: /p'
+sed -n '/^Filename: /p' Packages > Packages-files
+rm -f Packages
+find -type f -name "*Packages*" | xargs -L1 sed -i 's|Filename: |http://downloads.openpli.org/feeds/openpli-7-release/3rd-party/|g'
+wget --wait=1 -i Packages-files
