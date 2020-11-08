39bb6dd8810f3c793eff8a11b17876a94f0d686d
persianpros <persianpros@gmail.com>
2020-11-08 23:56:27 +0330

    Lets have modules

--- feed/get-ts.sh
+++ feed/get-ts.sh
@@ -6,7 +6,6 @@ wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tu
 wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_IPTV/
 wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_Backup/
 wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_Multiboot/
-rename 'y/A-Z/a-z/' *.ipk
 mv -f *.ipk ../
 cd ..
-ls -vr *.ipk | awk -F- '$1 == name{system ("rm \""$0"\"")}{name=$1}'
+rm -rf ts
