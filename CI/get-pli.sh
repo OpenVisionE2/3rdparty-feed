5935cf5dea63f3d49336962fb072f7015bfbd4ec
AYP <persianpros@yahoo.com>
2022-10-05 02:19:14 +0330

    Update get-pli.sh

--- CI/get-pli.sh
+++ CI/get-pli.sh
@@ -3,12 +3,12 @@
 cd feed
 mkdir -p pli
 cd pli
-wget http://downloads.openpli.org/feeds/openpli-8.2-release/3rd-party/Packages.gz
+wget http://downloads.openpli.org/feeds/openpli-8.3-release/3rd-party/Packages.gz
 gzip -d Packages.gz
 find -type f -name "*Packages*" | xargs -L1 sed -i '/^Filename: /p'
 sed -n '/^Filename: /p' Packages > Packages-files
 rm -f Packages
-find -type f -name "*Packages*" | xargs -L1 sed -i 's|Filename: |http://downloads.openpli.org/feeds/openpli-8.2-release/3rd-party/|g'
+find -type f -name "*Packages*" | xargs -L1 sed -i 's|Filename: |http://downloads.openpli.org/feeds/openpli-8.3-release/3rd-party/|g'
 wget --wait=1 -i Packages-files
 mv -f *.ipk ../
 cd ..
